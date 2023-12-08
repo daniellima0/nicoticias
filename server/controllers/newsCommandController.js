5fca42a0f0cdd52f66870e13a95ae1e6fbe7201c
orlandomotapires <orlandomota2002@gmail.com>
2023-12-08 10:25:24 -0300

    [FEAT]: Server side changes

--- server/controllers/newsCommandController.js
+++ server/controllers/newsCommandController.js
@@ -11,15 +11,6 @@ exports.createNews = async (req, res) => {
   }
 };
 
-exports.getAllNews = async (req, res) => {
-  try {
-    const news = await News.find();
-    res.status(200).json(news);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
 exports.deleteAllNews = async (req, res) => {
   try {
     await News.deleteMany({});
--- server/routes/newsRoutes.js
+++ server/routes/newsRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const newsController = require("../controllers/newsController");
 
-router.post("/", newsController.createNews);
-router.get("/", newsController.getAllNews);
-router.delete('/', newsController.deleteAllNews);
+const newsCommand = require("../controllers/newsCommandController");
+const newsQuery = require("../controllers/newsQueryController");
+
+router.post("/", newsCommand.createNews);
+router.get("/", newsQuery.getAllNews);
+router.delete('/', newsCommand.deleteAllNews);
 
 module.exports = router;
